33baa111379dd59c9d3cea720029f0c47a4fbc62
Thalita <119790076+FernandesThalita@users.noreply.github.com>
2023-06-01 09:38:54 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+io.on("connection", function (socket) {
+  socket.on("newuser", function (username) {
+    socket.broadcast.emit("uptade", username + "entrou no grupo");
+  });
+  socket.on("exituser", function (username) {
+    socket.broadcast.emit("uptade", username + "deixou o grupo");
+  });
+  socket.on("chat", function (message) {
+    socket.broadcast.emit("chat", message);
+  });
+});
+
+server.listen(3000)
+console.log(`http://localhost:3000`)
